457d9ff9eb94df079c199b12fc8d391a32d8c1e0
Francois Reynaud <francois.reynaud@gmail.com>
2012-07-30 07:38:18 +0200

    committing a failing test for criteria.If both A and B are present in the object tree, it would be good to have (A or B) returning A, and (B or A) returning B.

---
+++ server/src/test/java/org/uiautomation/ios/e2e/intl/CriteriaKeepsOrderTests.java
@@ -0,0 +1,42 @@
+package org.uiautomation.ios.e2e.intl;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.uiautomation.ios.UIAModels.UIAElement;
+import org.uiautomation.ios.UIAModels.UIATableCell;
+import org.uiautomation.ios.UIAModels.predicate.AndCriteria;
+import org.uiautomation.ios.UIAModels.predicate.Criteria;
+import org.uiautomation.ios.UIAModels.predicate.NameCriteria;
+import org.uiautomation.ios.UIAModels.predicate.OrCriteria;
+import org.uiautomation.ios.UIAModels.predicate.TypeCriteria;
+import org.uiautomation.ios.client.uiamodels.impl.RemoteUIADriver;
+import org.uiautomation.ios.client.uiamodels.impl.RemoteUIAWindow;
+import org.uiautomation.ios.server.application.Localizable;
+
+public class CriteriaKeepsOrderTests extends IntlMountainTestsBase {
+
+  @Test(groups="broken")
+  public void orCriteriaReturnsFirstMatch() {
+    RemoteUIADriver driver = null;
+    try {
+
+      driver = getDriver(Localizable.en);
+      RemoteUIAWindow win = getMainWindow(driver);
+      Criteria mountain1 = new NameCriteria("Mountain 1");
+      Criteria mountain2 = new NameCriteria("Mountain 2");
+
+      UIAElement result1 = win.findElement(new OrCriteria(mountain1, mountain2));
+      UIAElement result2 = win.findElement(new OrCriteria(mountain2, mountain1));
+
+      Assert.assertEquals(result1.getName(), "Mountain 1");
+      Assert.assertEquals(result2.getName(), "Mountain 2");
+
+
+
+    } finally {
+      if (driver != null) {
+        driver.quit();
+      }
+    }
+  }
+}
