6b343c1463beef4a86a454b0e6c50e2d8a1fdf89
Francois Reynaud <francois.reynaud@gmail.com>
2012-07-30 08:16:47 +0200

    adding a more complex OR to test recursion

--- server/src/test/java/org/uiautomation/ios/e2e/intl/CriteriaKeepsOrderTests.java
+++ server/src/test/java/org/uiautomation/ios/e2e/intl/CriteriaKeepsOrderTests.java
@@ -15,7 +15,7 @@ import org.uiautomation.ios.server.application.Localizable;
 
 public class CriteriaKeepsOrderTests extends IntlMountainTestsBase {
 
-  @Test(groups="broken")
+  @Test(groups = "broken")
   public void orCriteriaReturnsFirstMatch() {
     RemoteUIADriver driver = null;
     try {
@@ -30,6 +30,35 @@ public class CriteriaKeepsOrderTests extends IntlMountainTestsBase {
 
       Assert.assertEquals(result1.getName(), "Mountain 1");
       Assert.assertEquals(result2.getName(), "Mountain 2");
+
+
+
+    } finally {
+      if (driver != null) {
+        driver.quit();
+      }
+    }
+  }
+
+
+  @Test(groups = "broken")
+  public void complexOrCriteriaReturnsFirstMatch() {
+    RemoteUIADriver driver = null;
+    try {
+
+      driver = getDriver(Localizable.en);
+      RemoteUIAWindow win = getMainWindow(driver);
+      Criteria mountain1 = new NameCriteria("Mountain 1");
+      Criteria mountain2 = new NameCriteria("Mountain 2");
+      Criteria mountain3 = new NameCriteria("Mountain 3");
+
+
+      UIAElement result1 = win.findElement(new OrCriteria(mountain1, mountain2, mountain3));
+      UIAElement result3 =
+          win.findElement(new OrCriteria(new OrCriteria(mountain3, mountain2), mountain1));
+
+      Assert.assertEquals(result1.getName(), "Mountain 1");
+      Assert.assertEquals(result3.getName(), "Mountain 3");
 
 
 
