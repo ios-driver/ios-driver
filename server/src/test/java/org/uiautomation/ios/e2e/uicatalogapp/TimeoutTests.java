ae837e4d747420e87c66181eaeb57f117385727f
Francois Reynaud <francois.reynaud@gmail.com>
2012-08-06 16:31:35 +0200

    testing getters and setters

--- server/src/test/java/org/uiautomation/ios/e2e/uicatalogapp/TimeoutTests.java
+++ server/src/test/java/org/uiautomation/ios/e2e/uicatalogapp/TimeoutTests.java
@@ -1,18 +1,21 @@
 package org.uiautomation.ios.e2e.uicatalogapp;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.uiautomation.ios.client.uiamodels.impl.RemoteUIADriver;
 
-public class TimeoutTests  extends UICatalogTestsBase{
+public class TimeoutTests extends UICatalogTestsBase {
 
-  
+
   @Test
   public void getSetTimeout() throws InterruptedException {
     RemoteUIADriver driver = null;
     try {
 
       driver = getDriver();
+      Assert.assertEquals(driver.getLocalTarget().getTimeout(), 5);
       driver.getLocalTarget().setTimeout(6);
+      Assert.assertEquals(driver.getLocalTarget().getTimeout(), 6);
     } finally {
       if (driver != null) {
         driver.quit();
