8580d6ebb7b8508820cc1921a6272f01fb9ed923
Francois Reynaud <francois.reynaud@gmail.com>
2012-08-07 10:47:25 +0200

    making tests pass again

--- server/src/test/java/org/uiautomation/ios/e2e/uicatalogapp/TimeoutTests.java
+++ server/src/test/java/org/uiautomation/ios/e2e/uicatalogapp/TimeoutTests.java
@@ -2,7 +2,14 @@ package org.uiautomation.ios.e2e.uicatalogapp;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.uiautomation.ios.UIAModels.UIAElement;
+import org.uiautomation.ios.UIAModels.UIATableCell;
+import org.uiautomation.ios.UIAModels.predicate.AndCriteria;
+import org.uiautomation.ios.UIAModels.predicate.Criteria;
+import org.uiautomation.ios.UIAModels.predicate.NameCriteria;
+import org.uiautomation.ios.UIAModels.predicate.TypeCriteria;
 import org.uiautomation.ios.client.uiamodels.impl.RemoteUIADriver;
+import org.uiautomation.ios.client.uiamodels.impl.RemoteUIAWindow;
 
 public class TimeoutTests extends UICatalogTestsBase {
 
@@ -13,13 +20,40 @@ public class TimeoutTests extends UICatalogTestsBase {
     try {
 
       driver = getDriver();
-      Assert.assertEquals(driver.getLocalTarget().getTimeout(), 5);
-      driver.getLocalTarget().setTimeout(6);
-      Assert.assertEquals(driver.getLocalTarget().getTimeout(), 6);
+      Assert.assertEquals(driver.getLocalTarget().getTimeout(), 0);
+      driver.getLocalTarget().setTimeout(17);
+      Assert.assertEquals(driver.getLocalTarget().getTimeout(), 17);
+
+
     } finally {
       if (driver != null) {
         driver.quit();
       }
     }
   }
+
+  @Test
+  public void getElement() throws InterruptedException {
+    RemoteUIADriver driver = null;
+    try {
+
+      String name = "Buttons, Various uses of UIButton2";
+      driver = getDriver();
+      driver.getLocalTarget().setTimeout(10);
+      RemoteUIAWindow win = getMainWindow(driver);
+      Criteria c1 = new TypeCriteria(UIATableCell.class);
+      Criteria c2 = new NameCriteria(name);
+      Criteria c = new AndCriteria(c1, c2);
+      System.err.println("START");
+      UIAElement element = win.findElement(c);
+
+    } finally {
+      System.err.println("END");
+      if (driver != null) {
+        driver.quit();
+      }
+    }
+  }
+
+
 }
