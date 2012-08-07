14e8e2a237d14d21383510087c1415890ebebd8a
Francois Reynaud <francois.reynaud@gmail.com>
2012-08-07 10:46:12 +0200

    adding the correction for the time speeder before it reaches the instrument script.

--- server/src/main/java/org/uiautomation/ios/server/command/impl/CustomUIAScriptHandler.java
+++ server/src/main/java/org/uiautomation/ios/server/command/impl/CustomUIAScriptHandler.java
@@ -17,11 +17,13 @@ import org.json.JSONObject;
 import org.uiautomation.ios.communication.WebDriverLikeRequest;
 import org.uiautomation.ios.exceptions.IOSAutomationException;
 import org.uiautomation.ios.server.command.UIAScriptHandler;
+import org.uiautomation.ios.server.command.impl.session.hack.TimeSpeeder;
 import org.uiautomation.ios.server.instruments.SessionsManager;
 
 
 // TODO should go away eventually.
 public class CustomUIAScriptHandler extends UIAScriptHandler {
+
 
 
   private static final String capabilities =
@@ -31,7 +33,7 @@ public class CustomUIAScriptHandler extends UIAScriptHandler {
       "UIATarget.localTarget().tap({x::x,y::y});getJsonResult(':sessionId',0,'')";
 
   private static final String getTimeout =
-      "var timeout = UIAutomation.TIMEOUT_IN_SEC;UIAutomation.createJSONResponse(':sessionId',0,timeout)";
+      "var timeout = UIAutomation.TIMEOUT_IN_SEC / :correction;UIAutomation.createJSONResponse(':sessionId',0,timeout)";
 
   private static final String setTimeout =
       "UIAutomation.setTimeout(:timeout);UIAutomation.createJSONResponse(':sessionId',0,'')";
@@ -51,10 +53,17 @@ public class CustomUIAScriptHandler extends UIAScriptHandler {
         s = capabilities.replace(":sessionId", instruments.getCurrentSessionId());
         return s;
       case SET_TIMEOUT:
-        s = setTimeout.replace(":timeout", String.format("%d", r.getPayload().getInt("timeout")));
+        s =
+            setTimeout.replace(
+                ":timeout",
+                String.format("%f", r.getPayload().getInt("timeout")
+                    * TimeSpeeder.getInstance().getSecondDuration()));
         return s;
       case GET_TIMEOUT:
-        return getTimeout;
+        s =
+            getTimeout.replace(":correction",
+                String.format("%f", TimeSpeeder.getInstance().getSecondDuration()));
+        return s;
       case TARGET_TAP:
         JSONObject payload = getRequest().getPayload();
         s =
@@ -65,6 +74,4 @@ public class CustomUIAScriptHandler extends UIAScriptHandler {
         throw new IOSAutomationException("NI");
     }
   }
-
-
 }
