e3ac7a675b34b19f54bfd2d2ec58c6f32280ff7f
Francois Reynaud <francois.reynaud@gmail.com>
2012-08-15 14:10:53 +0200

    missing class

---
+++ client/src/main/java/org/uiautomation/ios/client/uiamodels/impl/RemoteUIAActivityIndicator.java
@@ -0,0 +1,11 @@
+package org.uiautomation.ios.client.uiamodels.impl;
+
+import org.uiautomation.ios.UIAModels.UIAActivityIndicator;
+
+public class RemoteUIAActivityIndicator extends RemoteUIAElement implements UIAActivityIndicator {
+
+  public RemoteUIAActivityIndicator(RemoteUIADriver driver, String reference) {
+    super(driver, reference);
+  }
+
+}
