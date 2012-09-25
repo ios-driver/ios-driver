8c7fbb3f2688718a030fe00e934947d9f8d1fd97
Roberto Gonzalez <rgonzlezfern@ebay.com>
2012-09-25 14:41:36 +0200

    css changes, menu in top

--- ide/src/main/java/org/uiautomation/ios/ide/views/IDEMainView.java
+++ ide/src/main/java/org/uiautomation/ios/ide/views/IDEMainView.java
@@ -86,7 +86,7 @@ public class IDEMainView implements View {
       b.append("<script >configure('" + d + "','" + model.getDeviceOrientation() + "');</script>");
       b.append("<script >resize();</script>");
 
-      b.append("<div id ='footer'>");
+      b.append("<div id ='topmenu'>");
       b.append("<div id=\"picture\"/>");
       b.append("<img src=\"" + getIcon() + "\"/>");
       b.append("</div>");
@@ -104,7 +104,7 @@ public class IDEMainView implements View {
 
       b.append("<div class=\"box\" id=\"box\">");
       b.append("<a class=\"boxclose\" id=\"boxclose\"></a>");
-      b.append("<h3>Capabilities</h3>");
+      b.append("<h4>Capabilities</h4>");
       b.append("<p>");
       b.append(displayCapabilities());
       b.append("</p>");
@@ -116,7 +116,7 @@ public class IDEMainView implements View {
 
       b.append("<div class=\"boxlanguages\" id=\"boxlanguages\">");
       b.append("<a class=\"boxcloselanguages\" id=\"boxcloselanguages\"></a>");
-      b.append("<h3>Supported Languages</h3>");
+      b.append("<h4>Supported Languages</h4>");
       b.append("<p>");
       b.append(getListOfLanguagesInHTML());
       b.append("</p>");
@@ -197,7 +197,7 @@ public class IDEMainView implements View {
     JSONObject app = getAppFromStatus();
     JSONObject resources = app.getJSONObject("resources");
     String icon = resources.getString("CFBundleIconFile");
-   
+
     String h = model.getEndPoint().getHost();
     int p = model.getEndPoint().getPort();
     return "http://" + h + ":" + p + icon;
