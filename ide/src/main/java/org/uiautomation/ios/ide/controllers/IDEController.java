6a52beebfebb40888e13ced37c9d1ca4be3ef15f
Roberto Gonzalez <rgonzlezfern@ebay.com>
2012-09-27 20:12:13 +0200

    now the image in the IDE is refreshed

--- ide/src/main/java/org/uiautomation/ios/ide/controllers/IDEController.java
+++ ide/src/main/java/org/uiautomation/ios/ide/controllers/IDEController.java
@@ -27,6 +27,7 @@ public class IDEController implements IDECommandController {
   public View handle(HttpServletRequest req) throws IOSAutomationException, Exception {
     final Session s = new Session(extractSession(req.getPathInfo()));
     IDESessionModel model = cache.getModel(s);
+    model.refresh();
     return new IDEMainView(model, req.getContextPath() + req.getServletPath());
   }
 
