8f9d8ac06721eb1c7deffcde64e0f12f6db25c11
Francois Reynaud <francois.reynaud@gmail.com>
2012-10-10 09:13:49 +0100

    braking isStale feature to make ios6 tests pass. isStale brakes something in UITableView. Probably the same as keyboard and key + scrolltoVisible

--- server/src/main/resources/UIAutomation.js
+++ server/src/main/resources/UIAutomation.js
@@ -52,9 +52,9 @@ var Cache = function() {
 			} else if(res.type && (res.type() == "UIAWindow" || res.type() == "UIAApplication")) {
 				return res;
 				// on arrays, stale doesn't make sense.
-			} else if(res.isStale && res.isStale()) {
+			} /*else if(res.isStale && res.isStale()) {
 			 throw new UIAutomationException("elements ref:" + reference + " is stale", 10);
-			 } else {
+			 } */else {
 				return res;
 			}
 		}
