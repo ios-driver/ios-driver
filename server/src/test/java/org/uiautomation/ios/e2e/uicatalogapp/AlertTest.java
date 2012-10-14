e806c28fd226b9295c471dcb3d365c373a719ef2
Francois Reynaud <francois.reynaud@gmail.com>
2012-10-14 18:58:53 +0100

    testing alerts.

--- server/src/test/java/org/uiautomation/ios/e2e/uicatalogapp/AlertTest.java
+++ server/src/test/java/org/uiautomation/ios/e2e/uicatalogapp/AlertTest.java
@@ -2,9 +2,12 @@ package org.uiautomation.ios.e2e.uicatalogapp;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.uiautomation.ios.UIAModels.UIAAlert;
+import org.uiautomation.ios.UIAModels.UIAButton;
 import org.uiautomation.ios.UIAModels.UIAElement;
+import org.uiautomation.ios.UIAModels.UIALink;
+import org.uiautomation.ios.UIAModels.UIAStaticText;
 import org.uiautomation.ios.UIAModels.UIATableCell;
-import org.uiautomation.ios.UIAModels.UIATextField;
 import org.uiautomation.ios.UIAModels.predicate.AndCriteria;
 import org.uiautomation.ios.UIAModels.predicate.Criteria;
 import org.uiautomation.ios.UIAModels.predicate.MatchingStrategy;
@@ -14,6 +17,7 @@ import org.uiautomation.ios.client.uiamodels.impl.RemoteUIAApplication;
 import org.uiautomation.ios.client.uiamodels.impl.RemoteUIADriver;
 import org.uiautomation.ios.client.uiamodels.impl.RemoteUIATarget;
 import org.uiautomation.ios.client.uiamodels.impl.RemoteUIAWindow;
+import org.uiautomation.ios.exceptions.NoAlertOpenError;
 import org.uiautomation.ios.exceptions.UnexpectedAlertOpen;
 
 public class AlertTest extends UICatalogTestsBase {
@@ -42,11 +46,57 @@ public class AlertTest extends UICatalogTestsBase {
       RemoteUIAWindow win = app.getMainWindow();
       getAlert(win);
 
-      UIAElement el = win.findElements(new NameCriteria("Show Simple")).get(2);
+      Criteria c =
+          new AndCriteria(new TypeCriteria(UIAStaticText.class), new NameCriteria("Show Simple"));
+      UIAElement el = win.findElements(c).get(1);
       // opens an alert.
       el.tap();
       // should throw.The alert prevent interacting with the background.
       el.tap();
+
+    } finally {
+      if (driver != null) {
+        driver.quit();
+      }
+    }
+  }
+
+  @Test
+  public void checkUIAlertView() throws Exception {
+    RemoteUIADriver driver = null;
+    try {
+
+      driver = getDriver();
+      RemoteUIATarget target = driver.getLocalTarget();
+      RemoteUIAApplication app = target.getFrontMostApp();
+      RemoteUIAWindow win = app.getMainWindow();
+      getAlert(win);
+
+      Criteria c =
+          new AndCriteria(new TypeCriteria(UIAStaticText.class), new NameCriteria("Show Simple"));
+      UIAElement el = win.findElements(c).get(1);
+      // opens an alert.
+      el.tap();
+
+
+      UIAAlert alert = target.getAlert();
+
+      // check the alert has all its elements
+      alert.findElement(UIAStaticText.class, new NameCriteria("UIAlertView"));
+      alert.findElement(UIAStaticText.class, new NameCriteria("<Alert message>"));
+      UIAButton ok = alert.findElement(UIAButton.class, new NameCriteria("OK"));
+
+      ok.tap();
+
+      
+      Exception expected = null;
+      try {
+        target.getAlert();
+      } catch (NoAlertOpenError e) {
+        expected = e;
+      }
+      Assert.assertNotNull(expected);
+      Assert.assertEquals(expected.getClass(), NoAlertOpenError.class);
 
 
 
