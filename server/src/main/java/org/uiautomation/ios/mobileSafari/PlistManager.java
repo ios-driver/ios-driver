3e99a96f763a72d8d580a7efa2cc3f3d09f7d932
Francois Reynaud <francois.reynaud@gmail.com>
2012-11-21 09:46:02 +0000

    removing the code using plutil, too slow.

--- server/src/main/java/org/uiautomation/ios/mobileSafari/PlistManager.java
+++ server/src/main/java/org/uiautomation/ios/mobileSafari/PlistManager.java
@@ -18,6 +18,14 @@ import org.json.JSONObject;
 import org.uiautomation.ios.exceptions.IOSAutomationException;
 import org.uiautomation.ios.exceptions.IOSAutomationSetupException;
 
+import com.dd.plist.BinaryPropertyListParser;
+import com.dd.plist.BinaryPropertyListWriter;
+import com.dd.plist.NSArray;
+import com.dd.plist.NSDictionary;
+import com.dd.plist.NSObject;
+import com.dd.plist.PropertyListParser;
+import com.dd.plist.XMLPropertyListParser;
+
 public class PlistManager {
 
   public static final String SET_CONNECTION_KEY = "webinspector/setConnectionKey.xml";
@@ -25,21 +33,28 @@ public class PlistManager {
   public static final String SET_SENDER_KEY = "webinspector/setSenderKey.xml";
   public static final String SEND_JSON_COMMAND = "webinspector/sendJSONCommand.xml";
 
+  private static String cacheTemplate = loadFromTemplate(SEND_JSON_COMMAND);
 
+  public static String loadFromTemplate(String templatePath) {
+    if (SEND_JSON_COMMAND.equals(templatePath) && cacheTemplate != null) {
+      return cacheTemplate;
+    }
 
-  public String loadFromTemplate(String templatePath) throws IOException {
-    InputStream is =
-        Thread.currentThread().getContextClassLoader().getResourceAsStream(templatePath);
+    InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(templatePath);
     if (is == null) {
       throw new IOSAutomationSetupException("cannot load : " + templatePath);
     }
     StringWriter writer = new StringWriter();
-    IOUtils.copy(is, writer, "UTF-8");
+    try {
+      IOUtils.copy(is, writer, "UTF-8");
+    } catch (IOException e) {
+      throw new IOSAutomationSetupException("Cannot load template " + templatePath);
+    }
     String content = writer.toString();
     return content;
   }
-  
-  public String JSONCommand(JSONObject command) throws IOException{
+
+  public String JSONCommand(JSONObject command) throws IOException {
     String json = command.toString();
     String s = Base64.encodeBase64String(json.getBytes());
     String template = loadFromTemplate(SEND_JSON_COMMAND);
@@ -47,68 +62,14 @@ public class PlistManager {
     return res;
   }
 
-  private byte[] convertPlist(byte[] src, String outputType) throws IOException,
-      InterruptedException {
-    ProcessBuilder pb =
-        new ProcessBuilder("/usr/bin/plutil", "-convert", outputType, "-", "-o", "-");
-    Process process = pb.start();
-    process.getOutputStream().write(src);
-    process.getOutputStream().close();
-    int exitCode = process.waitFor();
-    //System.err.println("plutil exit code: " + exitCode);
-    byte[] bytes = IOUtils.toByteArray(process.getInputStream());
-    return bytes;
-  }
-  
-  private File createTmpFile(String xmlContent) throws IOException {
-    File res = File.createTempFile("global", ".xml");
-    BufferedWriter out = new BufferedWriter(new FileWriter(res));
-    out.write(xmlContent);
-    out.close();
-    return res;
+  public byte[] plistXmlToBinary(String msg) throws Exception {
+    NSObject object = XMLPropertyListParser.parse(msg.getBytes());
+    return BinaryPropertyListWriter.writeToArray(object);
+
   }
 
-  private File  writeBinaryPListFromXML(String xmlContent) throws IOException {
-        File from = createTmpFile(xmlContent);
-        File to = File.createTempFile("debug", ".plist");
-
-    List<String> command = new ArrayList<String>();
-    command.add("/usr/bin/plutil");
-    command.add("-convert");
-    command.add("binary1");
-    command.add("-o");
-    command.add(to.getAbsolutePath());
-    command.add(from.getAbsolutePath());
-
-    ProcessBuilder b = new ProcessBuilder(command);
-    try {
-      Process p = b.start();
-      int i = p.waitFor();
-      if (i != 0) {
-        throw new IOSAutomationException("convertion to binary pfile failed.exitCode=" + i);
-      }
-    } catch (Exception e) {
-      throw new IOSAutomationException("failed to run " + command.toString(), e);
-    }
-    return to;
-  }
-  
-  private byte[] getBytes(File f) throws FileNotFoundException, IOException{
-    return IOUtils.toByteArray(new FileInputStream(f));
-  }
-  
-  
-
-  public byte[] plistXmlToBinary(String msg) throws IOException, InterruptedException {
-    //return convertPlist(msg.getBytes(), "binary1");
-    File xml = writeBinaryPListFromXML(msg);
-    return getBytes(xml);
-    
-  }
-
-  public String plistBinaryToXml(byte[] binary) throws IOException, InterruptedException {
-    //System.err.println("Converting " + binary.length + " bytes to XML.");
-    byte[] bytes = convertPlist(binary, "xml1");
-    return new String(bytes);
+  public String plistBinaryToXml(byte[] binary) throws Exception {
+    NSObject object = BinaryPropertyListParser.parse(binary);
+    return object.toXMLPropertyList();
   }
 }
