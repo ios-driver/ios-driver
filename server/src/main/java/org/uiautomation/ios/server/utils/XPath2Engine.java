d72cc23de1996bf19ab8baa6172ce04f0da96891
Francois Reynaud <francois.reynaud@gmail.com>
2012-12-01 17:28:28 +0000

    xpath with saxon

--- server/src/main/java/org/uiautomation/ios/server/utils/XPath2Engine.java
+++ server/src/main/java/org/uiautomation/ios/server/utils/XPath2Engine.java
@@ -1,118 +1,116 @@
 package org.uiautomation.ios.server.utils;
 
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
-import org.dom4j.InvalidXPathException;
-import org.dom4j.Node;
-import org.dom4j.XPathException;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import net.sf.saxon.dom.DOMNodeList;
+import net.sf.saxon.lib.NamespaceConstant;
+
 import org.openqa.selenium.InvalidSelectorException;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriverException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 
 import com.google.common.collect.ImmutableMap;
 
-public class JSONTreeParser {
+public class XPath2Engine {
 
-  private final Document xml;
+  private final XPath xpath2;
+  private final Node document;
 
-  public JSONTreeParser(JSONObject tree) {
-    xml = buildXMLDoc(tree);
+  public XPath2Engine(String xml) throws Exception {
+    String objectModel = NamespaceConstant.OBJECT_MODEL_SAXON;
+    System.setProperty("javax.xml.xpath.XPathFactory:" + objectModel, "net.sf.saxon.xpath.XPathFactoryImpl");
+
+    XPathFactory xpathFactory = XPathFactory.newInstance(objectModel);
+    xpath2 = xpathFactory.newXPath();
+    DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
+
+    DocumentBuilder docBuilder = dfactory.newDocumentBuilder();
+    document = docBuilder.parse(new InputSource(new StringReader(xml)));
   }
 
   public List<Map<String, String>> findElementsByXpath(String xpath) {
-    return findElementsByXpath(xpath, xml);
+    try {
+      return findElementsByXpath(xpath, document);
+    } catch (XPathExpressionException e) {
+      throw new WebDriverException(e);
+    }
   }
 
   public Map<String, String> findElementByXpath(String xpath) {
-    return findElementByXpath(xpath, xml);
-  }
-  
-  public Map<String, String> findElementByXpath(String xpath,String reference) {
-    System.out.println(xml.asXML());
-    return findElementByXpath(xpath, getNode(reference));
-  }
-  
-  public List<Map<String, String>> findElementsByXpath(String xpath, String reference) {
-    return findElementsByXpath(xpath, getNode(reference));
-  }
-
-  private List<Map<String, String>> findElementsByXpath(String xpath, Node from) {
-    List<Element> elements;
     try {
-      elements = (List<Element>) xml.selectNodes(xpath);
-    } catch (InvalidXPathException e) {
-      throw new InvalidSelectorException(e.getMessage());
-    } catch (XPathException e) {
-      throw new InvalidSelectorException(e.getMessage());
+      return findElementByXpath(xpath, document);
+    } catch (XPathExpressionException e) {
+      throw new InvalidSelectorException("wrong xpath " + xpath, e);
     }
-    List<Map<String, String>> res = new ArrayList<Map<String, String>>();
-    for (Element el : elements) {
-      String reference = el.attributeValue("ref");
-      String type = el.getName();
-      res.add(ImmutableMap.of("ELEMENT", reference, "type", type));
+  }
 
+  public Map<String, String> findElementByXpath(String xpath, String reference) {
+    try {
+      return findElementByXpath(xpath, getNode(reference));
+    } catch (XPathExpressionException e) {
+      throw new InvalidSelectorException("wrong xpath " + xpath, e);
     }
+
+  }
+
+  public List<Map<String, String>> findElementsByXpath(String xpath, String reference) {
+    try {
+      return findElementsByXpath(xpath, getNode(reference));
+    } catch (XPathExpressionException e) {
+      throw new InvalidSelectorException("wrong xpath " + xpath, e);
+    }
+  }
+
+  private List<Map<String, String>> findElementsByXpath(String xpath, Node from) throws XPathExpressionException {
+    XPathExpression expr = xpath2.compile(xpath);
+    DOMNodeList elements;
+
+    elements = (DOMNodeList) expr.evaluate(document, XPathConstants.NODESET);
+
+    List<Map<String, String>> res = new ArrayList<Map<String, String>>();
+    for (int i = 0; i < elements.getLength(); i++) {
+      Element el = (Element) elements.item(i);
+      String reference = el.getAttribute("ref");
+      String type = el.getNodeName();
+      res.add(ImmutableMap.of("ELEMENT", reference, "type", type));
+    }
+
     return res;
   }
 
-  private Map<String, String> findElementByXpath(String xpath, Node from) {
-
-    Element element = null;
-    try {
-      element = (Element) from.selectSingleNode(xpath);
-    } catch (InvalidXPathException e) {
-      throw new InvalidSelectorException(e.getMessage());
-    } catch (XPathException e) {
-      throw new InvalidSelectorException(e.getMessage());
+  private Map<String, String> findElementByXpath(String xpath, Node from) throws XPathExpressionException {
+    XPathExpression expr = xpath2.compile(xpath);
+    Element el = (Element) expr.evaluate(document, XPathConstants.NODE);
+    if (el == null) {
+      throw new NoSuchElementException("No element for xpath " + xpath);
     }
-
-    if (element == null) {
-      throw new NoSuchElementException("no element matching xpath=" + xpath);
-    }
-    String reference = element.attributeValue("ref");
-    String type = element.getName();
+    String reference = el.getAttribute("ref");
+    String type = el.getNodeName();
     return ImmutableMap.of("ELEMENT", reference, "type", type);
   }
 
-  private Node getNode(String reference) {
+  private Node getNode(String reference) throws XPathExpressionException {
     String xpath = "//*[@ref=" + reference + "]";
-    Node n = xml.selectSingleNode(xpath);
+    XPathExpression expr = xpath2.compile(xpath);
+    Node n = (Node) expr.evaluate(document, XPathConstants.NODE);
     if (n == null) {
       throw new NoSuchElementException("Element with ref " + reference + " doesn't exist.");
     }
     return n;
   }
 
-  private Document buildXMLDoc(JSONObject tree) {
-    Document document = DocumentHelper.createDocument();
-    Element root = document.addElement("root");
-    buildXMLNode(tree, root);
-    return document;
-
-  }
-
-  private static void buildXMLNode(JSONObject from, Element parent) {
-    if (from == null) {
-      return;
-    }
-    Element node = parent.addElement(from.optString("type"))
-        .addAttribute("name", from.optString("name"))
-        .addAttribute("label", from.optString("label"))
-        .addAttribute("value", from.optString("value"))
-        .addAttribute("ref", from.optString("ref"));
-    
-    JSONArray array = from.optJSONArray("children");
-    if (array != null) {
-      for (int i = 0; i < array.length(); i++) {
-        JSONObject n = array.optJSONObject(i);
-        buildXMLNode(n, node);
-      }
-    }
-  }
 }
