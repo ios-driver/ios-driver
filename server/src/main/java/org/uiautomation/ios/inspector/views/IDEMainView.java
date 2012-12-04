397543ad2a38fdcb263bd6eef3ceaedf3355960d
Roberto Gonzalez <rgonzlezfern@ebay.com>
2012-12-04 19:53:37 +0100

    see capabilities as the only menu on the top. More space for tree /details

--- server/src/main/java/org/uiautomation/ios/inspector/views/IDEMainView.java
+++ server/src/main/java/org/uiautomation/ios/inspector/views/IDEMainView.java
@@ -92,8 +92,6 @@ public class IDEMainView implements View {
       b.append("</div>");
       b.append("<ul>");
       b.append("<li id=\"capabilities\"><a href=\"#\">See Capabilities</a></li>");
-      b.append("<li id=\"languages\"><a href=\"#\">Suported Languages</a></li>");
-      b.append("<li><a href=\"#\">Debug Options</a></li>");
       b.append("</ul>");
       b.append("</div>");
 
@@ -101,28 +99,19 @@ public class IDEMainView implements View {
        * OVERLAY Capabilities
        */
       b.append("<div class=\"overlay\" id=\"overlay\" style=\"display:none;\"></div>");
-
       b.append("<div class=\"box\" id=\"box\">");
       b.append("<a class=\"boxclose\" id=\"boxclose\"></a>");
       b.append("<h4>Capabilities</h4>");
       b.append("<p>");
       b.append(displayCapabilities());
       b.append("</p>");
-      b.append("</div>");
-      /* END OVERLAY CAPABILITIES */
-
-      /* OVERLAY LANGUAGES */
-      b.append("<div class=\"overlaylanguages\" id=\"overlayLanguages\" style=\"display:none;\"></div>");
-
-      b.append("<div class=\"boxlanguages\" id=\"boxlanguages\">");
-      b.append("<a class=\"boxcloselanguages\" id=\"boxcloselanguages\"></a>");
       b.append("<h4>Supported Languages</h4>");
       b.append("<p>");
       b.append(getListOfLanguagesInHTML());
       b.append("</p>");
       b.append("</div>");
+      /* END OVERLAY CAPABILITIES */
 
-      /* END OVERLAY FOR LANGUAGES */
       b.append("</body>");
       b.append("</html>");
 
