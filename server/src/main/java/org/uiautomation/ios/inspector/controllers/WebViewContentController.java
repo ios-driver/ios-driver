20fedd5db3dd42b982b6f63647bca5a39163462c
Roberto Gonzalez <rgonzlezfern@ebay.com>
2012-12-07 15:21:41 +0100

    web Inspector displaying the web stored in the cache

--- server/src/main/java/org/uiautomation/ios/inspector/controllers/WebViewContentController.java
+++ server/src/main/java/org/uiautomation/ios/inspector/controllers/WebViewContentController.java
@@ -2,6 +2,8 @@ package org.uiautomation.ios.inspector.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.uiautomation.ios.UIAModels.Session;
 import org.uiautomation.ios.inspector.model.Cache;
 import org.uiautomation.ios.inspector.model.IDESessionModel;
@@ -15,7 +17,7 @@ public class WebViewContentController implements IDECommandController {
 
 	public WebViewContentController(Cache cache) {
 		this.cache = cache;
-		this.html = "<html>  <head>    <title>Tutorial: HelloWorld</title>  </head>  <body>    <h1>HelloWorld Web Inspector</h1></body></html>";
+
 	}
 
 	@Override
@@ -26,11 +28,60 @@ public class WebViewContentController implements IDECommandController {
 
 	@Override
 	public View handle(HttpServletRequest req) throws Exception {
+		this.html = "";
+
 		final Session s = new Session(extractSession(req.getPathInfo()));
 		IDESessionModel model = cache.getModel(s);
-		this.html = model.getTree().toString();
-		// mock to check that this code is served in the IDE
+		JSONObject tree = (JSONObject) model.getTree().get("tree");
+		this.html = findSourceHTML(tree);
 		return new WebView(this.html);
+	}
+
+
+	private String findSourceHTML(JSONObject node) throws Exception {
+		String[] keys = JSONObject.getNames(node);
+		try {
+			for (int i = 0; i < keys.length; i++) {
+				if (keys[i].equals("source")) {
+					return node.get(keys[i]).toString();
+				} else {
+					Object child = node.get(keys[i]);
+					if (child instanceof JSONArray) {
+						if (((JSONArray)child).length()> 0) {
+							String result = findSourceHTML((JSONArray)child);
+							if (result != null) {
+								return result;
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()
+					+ "  Failure when searching the node:" + node
+					+ " .Looking for the webView Source html");
+		}
+		return null;
+	}
+
+	private String findSourceHTML(JSONArray array) throws Exception {
+		try {
+			for (int i = 0; i < array.length(); i++) {
+				if(array.get(i) instanceof JSONObject){
+					if (array.get(i)!=null) {
+						String result = findSourceHTML((JSONObject)array.get(i));
+						if (result != null) {
+							return result;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()
+					+ "  Failure when searching in the Array:" + array
+					+ " .Looking for the webView Source html");
+		}
+		return null;
 	}
 
 	private String extractSession(String pathInfo) {
