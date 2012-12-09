8fa54b279abccb34eb2643fa1fbc8db4406d5c26
Francois Reynaud <francois.reynaud@gmail.com>
2012-12-09 19:15:42 +0000

    intelliJ debugging + maven + eclipse should all work now.

--- ios-selenium-tests/src/test/java/org/openqa/selenium/testing/InProject.java
+++ ios-selenium-tests/src/test/java/org/openqa/selenium/testing/InProject.java
@@ -15,10 +15,16 @@ public class InProject {
    *          wrapped FileNotFoundException if file could not be found
    */
   public static File locate(String path) {
+    // TODO freynaud find why intelliJ is different from eclipse + maven.
     File dir = new File("ios-selenium-tests").getAbsoluteFile();
     dir = new File(dir, "src/test/resources/copyFromSeleniumProject");
     File needle = new File(dir, path);
 
+    if (!needle.exists()) {
+      dir = new File(".").getAbsoluteFile();
+      dir = new File(dir, "src/test/resources/copyFromSeleniumProject");
+      needle = new File(dir, path);
+    }
     if (!needle.exists()) {
       throw new WebDriverException("Cannot load " + needle);
     }
