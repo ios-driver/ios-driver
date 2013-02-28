c9d8a29c54268ca83c62653f0ea3fd83856a6910
Luke Inman-Semerau <luke.semerau@gmail.com>
2013-02-27 16:03:46 -0800

    If a hybrid app has more than one UIWebView we should switch to at least the first one without failing. TODO: be able to handle multiple UIWebViews to switch to.
    Test case for isSelecting not working right after selecting.

--- ios-selenium-tests/src/test/java/org/uiautomation/ios/selenium/FormHandlingTest.java
+++ ios-selenium-tests/src/test/java/org/uiautomation/ios/selenium/FormHandlingTest.java
@@ -1,21 +1,15 @@
 package org.uiautomation.ios.selenium;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
+import static org.openqa.selenium.TestWaiter.waitFor;
+import static org.openqa.selenium.WaitingConditions.pageTitleToBe;
+import static org.testng.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.openqa.selenium.TestWaiter.waitFor;
-import static org.openqa.selenium.WaitingConditions.pageTitleToBe;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.testng.annotations.Test;
 
 public class FormHandlingTest extends BaseSeleniumTest {
 
@@ -320,6 +314,15 @@ public class FormHandlingTest extends BaseSeleniumTest {
     value = element.getAttribute("value");
 
     assertEquals(value.length(), (0));
+  }
+  
+  @Test
+  public void testIsSelectedShouldWorkRightAfterSelecting() {
+      driver.get(pages.formPage);
+      WebElement checkbox = driver.findElement(By.id("checky"));
+      checkbox.click();
+      // NOTE: works if one waits a few seconds: Thread.sleep(5000);
+      assertTrue(checkbox.isSelected());
   }
 
   @Test(enabled = false)
