7e1225eaf15b674152e40408bf41cccf045c8959
Francois Reynaud <francois.reynaud@gmail.com>
2013-07-22 16:02:32 +0200

    adding TODO. This config is just wrong.

--- server/src/main/java/org/uiautomation/ios/server/configuration/Configuration.java
+++ server/src/main/java/org/uiautomation/ios/server/configuration/Configuration.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriverException;
 
 public class Configuration {
 
+  // TODO freynaud extract to method checking sdk ?
   public static boolean SIMULATORS_ENABLED = false;
   public static boolean BETA_FEATURE = false;
 
