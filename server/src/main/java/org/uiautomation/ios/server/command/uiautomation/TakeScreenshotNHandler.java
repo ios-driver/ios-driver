0ae27b161591e56456f0cc22974af9396a83b464
Francois Reynaud <francois.reynaud@gmail.com>
2013-07-30 17:06:36 +0200

    refactoring screenshot taking capabilities. LogElementTree still to be done.

--- server/src/main/java/org/uiautomation/ios/server/command/uiautomation/TakeScreenshotNHandler.java
+++ server/src/main/java/org/uiautomation/ios/server/command/uiautomation/TakeScreenshotNHandler.java
@@ -23,14 +23,10 @@ import org.uiautomation.ios.communication.WebDriverLikeRequest;
 import org.uiautomation.ios.server.IOSServerManager;
 import org.uiautomation.ios.server.command.PostHandleDecorator;
 import org.uiautomation.ios.server.command.UIAScriptHandler;
-import org.uiautomation.ios.utils.FileTo64EncodedStringUtils;
+import org.uiautomation.ios.utils.InstrumentsGeneratedImage;
+import org.uiautomation.ios.utils.JSONWireImage;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class TakeScreenshotNHandler extends UIAScriptHandler {
 
@@ -39,7 +35,7 @@ public class TakeScreenshotNHandler extends UIAScriptHandler {
   String
       jsTemplate =
       "UIATarget.localTarget().captureScreenWithName('" + SCREEN_NAME + "');"
-      + "UIAutomation.createJSONResponse(':sessionId',0,'ok');";
+      + "UIAutomation.createJSONResponse(':sessionId',0,UIATarget.localTarget().getDeviceOrientation());";
 
   public TakeScreenshotNHandler(IOSServerManager driver, WebDriverLikeRequest request) {
     super(driver, request);
@@ -56,82 +52,30 @@ public class TakeScreenshotNHandler extends UIAScriptHandler {
     @Override
     public void decorate(Response response) {
 
-      String
-          path =
-          getDriver().getSession(getRequest().getSession()).getOutputFolder() + "/Run 1/"
-          + SCREEN_NAME
-          + ".png";
-
-
-      int rotateDegrees = 0;
-      Orientation orientation = getDriver().getSession(getRequest().getSession()).getNativeDriver().getNativeOrientation();
-
-      switch(orientation)
-      {
-        case LANDSCAPE:
-          rotateDegrees = 270;
-          break;
-        case UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT:
-          rotateDegrees = 90;
-          break;
-        case UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN:
-          rotateDegrees = 180;
-          break;
-      }
-
-      if (rotateDegrees != 0) {
-        try {
-          final File source = new File(path);
-
-          // It's possible the screenshot hasn't saved to disk yet, so we'll wait up to 5 seconds for that to happen.
-          int waitTimeInMs = 5000;
-          while (! source.exists() && (waitTimeInMs >= 0)) {
-            try {
-              Thread.sleep(100);
-            } catch (InterruptedException e) {
-              throw new RuntimeException(e);
-            }
-            waitTimeInMs -= 100;
-          }
-
-          final BufferedImage originalImage = ImageIO.read(source);
-
-          final BufferedImage rotated;
-          if (rotateDegrees == 90 || rotateDegrees == 270) {
-            rotated = new BufferedImage(originalImage.getHeight(), originalImage.getWidth(), originalImage.getType());
-          } else {
-            rotated = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), originalImage.getType());
-          }
-
-          // Rotate the image and then move it back up to the origin through a translation call, since it'll pivot around
-          // the center point which will cause non-square images to offset by the different in height and width.
-          final Graphics2D graphics = rotated.createGraphics();
-          graphics.rotate(Math.toRadians(rotateDegrees), rotated.getWidth() / 2, rotated.getHeight() / 2);
-          graphics.translate((rotated.getWidth() - originalImage.getWidth()) / 2, (rotated.getHeight() - originalImage.getHeight()) / 2);
-          graphics.drawImage(originalImage, 0, 0, originalImage.getWidth(), originalImage.getHeight(), null);
-
-          // Overwrite the original screenshot file so the rest of the screenshot handler functions as if the image were never rotated.
-          final FileOutputStream out = new FileOutputStream(path);
-          ImageIO.write(rotated, "PNG", out);
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-      }
-
-      File source = new File(path);
-      FileTo64EncodedStringUtils encoder = new FileTo64EncodedStringUtils(source);
+      String orientation = response.getValue().toString();
+      Orientation o;
       try {
-        String content64 = encoder.encode();
-        source.delete();
-        // JSONObject value = new JSONObject();
-        // value.put("64encoded", content64);
+        o = Orientation.valueOf(orientation);
+      } catch (IllegalArgumentException e) {
+        throw new WebDriverException(
+            "the response for the screenshot command should return the orientation the device was on"
+            + " when the screenshot was take.The orientation returned : "
+            + orientation + " isn't a valid orientation.");
+      }
+
+      String session = getRequest().getSession();
+      String folder = getDriver().getSession(session).getOutputFolder() + "/Run 1/";
+
+      File source = new File(folder, SCREEN_NAME + ".png");
+
+      try {
+        JSONWireImage image = new InstrumentsGeneratedImage(source, o);
+        String content64 = image.getAsBase64String();
         response.setValue(content64);
       } catch (Exception e) {
         throw new WebDriverException(
             "Error converting " + source.getAbsolutePath() + " to a 64 encoded string "
             + e.getMessage(), e);
-      } finally {
-        source.delete();
       }
 
     }
---
+++ server/src/main/java/org/uiautomation/ios/utils/JSONWireImage.java
@@ -0,0 +1,9 @@
+package org.uiautomation.ios.utils;
+
+/**
+ * format of images that are send over the network using the JSONWire protocol.
+ */
+public interface JSONWireImage {
+
+  public String getAsBase64String();
+}
