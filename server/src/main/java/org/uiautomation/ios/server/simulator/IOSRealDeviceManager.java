ccb3eb4b3ab6840a5dfe1d438018535e0e221a1d
Francois Reynaud <francois.reynaud@gmail.com>
2013-08-04 07:32:14 +0200

    removing hardcoded string

--- server/src/main/java/org/uiautomation/ios/server/simulator/IOSRealDeviceManager.java
+++ server/src/main/java/org/uiautomation/ios/server/simulator/IOSRealDeviceManager.java
@@ -100,7 +100,7 @@ public class IOSRealDeviceManager implements IOSDeviceManager {
 
   @Override
   public void resetContentAndSettings() {
-    device.emptyApplicationCache("com.ebay.iphone");
+    device.emptyApplicationCache(bundleId);
   }
 
   @Override
