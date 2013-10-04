a45b78771e00cd6b877d643df6e44a9717dc015d
Kevin Menard <nirvdrum@gmail.com>
2013-10-03 23:25:56 -0400

    Updated for the 0.6.5 release.

--- javascripts/properties.js
+++ javascripts/properties.js
@@ -14,8 +14,8 @@
 
 $(document).ready(function ($) {
 
-    var version = "0.6.3";
-    var downloadFile = "https://github.com/ios-driver/ios-driver/releases/download/0.6.3/ios-server-0.6.3-jar-with-dependencies.jar";
+    var version = "0.6.5";
+    var downloadFile = "https://github.com/ios-driver/ios-driver/releases/download/0.6.5/ios-server-0.6.5-jar-with-dependencies.jar";
     var github = "https://github.com/ios-driver/ios-driver/";
 
     $("#download_me").attr('href', downloadFile);
