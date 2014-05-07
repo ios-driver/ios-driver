5456d99e6cf793c27f76d5c1cb533e80abeb7c0f
freynaud <francois.reynaud@ebay.com>
2014-05-07 13:48:08 +0200

    for hybrid app, when webkit is corrupted, it can be recovered from by reseting the wkrdp

--- server/src/main/java/org/uiautomation/ios/context/BaseWebInspector.java
+++ server/src/main/java/org/uiautomation/ios/context/BaseWebInspector.java
@@ -31,6 +31,7 @@ import org.uiautomation.ios.server.ServerSideSession;
 import org.uiautomation.ios.wkrdp.ConnectListener;
 import org.uiautomation.ios.wkrdp.MessageListener;
 import org.uiautomation.ios.wkrdp.RemoteExceptionException;
+import org.uiautomation.ios.wkrdp.WebKitSeemsCorruptedException;
 import org.uiautomation.ios.wkrdp.command.DOM;
 import org.uiautomation.ios.wkrdp.command.Page;
 import org.uiautomation.ios.wkrdp.events.ChildNodeRemoved;
@@ -109,6 +110,7 @@ public abstract class BaseWebInspector implements MessageListener, ConnectListen
         log.warning("Exception waiting for ready state, nodeId=" +
             ((element != null) ? element.getNodeId() : "null") + ": " + e +
             ". Retrying ...");
+        throw new WebKitSeemsCorruptedException();
       }
     }
     return element;
